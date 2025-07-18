48d94685a8687b65e843778fc23812ab007c1ced
Omar Tamer <omartamerdev@gmail.com>
2025-07-18 18:27:53 +0300

    Added CI/CD

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -13,3 +13,8 @@ output "rds_db_name" {
 output "rds_password_secret_arn" {
   value = aws_db_instance.rds_postgresql.master_user_secret[0].secret_arn
 }
+
+
+output "ecr_repo_url" {
+  value = module.computes.ecr_repo_url
+}
